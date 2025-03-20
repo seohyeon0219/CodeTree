f12aa8c4c1c73ae0a577a6b2b297f05efd352f2f
seohyeon0219 <seohyeon0219@naver.com>
2025-03-20 11:38:46 +0900

    Title: 369 게임, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/369 게임/369-game.py
+++ 250320/369 게임/369-game.py
@@ -5,13 +5,13 @@ while i <= n:
     if i % 3 == 0:
         print(0, end=' ')
         i += 1
-    elif i % 10 == (3 or 6 or 9):
+    elif (i % 10 == 3) or (i % 10 == 6) or (i % 10 == 9):
         print(0, end=' ')
         i += 1
-    elif i // 10 == (3 or 6 or 9):
+    elif (i // 10 == 3) or (i // 10 == 6) or (i // 10 == 9):
         print(0, end=' ')
         i += 1
     else:
         print(i, end=' ')
         i += 1
-        # 3 6 9 13 16 19 
+    
