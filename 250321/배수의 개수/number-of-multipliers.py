fe37ee276a47268e979fa0efbc53115f64e62d86
seohyeon0219 <seohyeon0219@naver.com>
2025-03-21 14:08:57 +0900

    Title: 배수의 개수, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/배수의 개수/number-of-multipliers.py
+++ 250321/배수의 개수/number-of-multipliers.py
@@ -1,15 +1,16 @@
 cnt_3 = 0
 cnt_5 = 0
+n = 0
 
 for i in range(10):
     n = int(input())
-    if n % 3 == 0:
+
+    if (n % 3 == 0) and (n % 5 == 0):
         cnt_3 += 1
-        if n % 5 == 0:
-            cnt_5 += 1
+        cnt_5 += 1
+    elif n % 3 == 0:
+        cnt_3 += 1
     elif n % 5 == 0:
         cnt_5 += 1
-        if n % 3 == 0:
-            cnt_3 += 1
 
 print(cnt_3, cnt_5)
