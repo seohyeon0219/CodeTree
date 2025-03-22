c67ca1b54690c9eed2c0d0807e78002af91defae
seohyeon0219 <seohyeon0219@naver.com>
2025-03-22 17:34:04 +0900

    Title: A부터 B까지 짝수의 합, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250322/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.py
@@ -0,0 +1,13 @@
+a, b = map(int,input().split())
+sum = 0
+
+if a < b:
+    for i in range(a, b+1):
+        if i % 2 == 0:
+            sum += i
+else:
+    for i in range(b, a+1):
+        if i % 2 == 0:
+            sum += i
+
+print(sum)
