4b4d3f7f219369e138279c105b1be8243909972e
seohyeon <seohyeon0219@naver.com>
2025-03-23 17:37:07 +0900

    Title: 조건에 부합하는 수, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250323/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 250323/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -3,9 +3,12 @@ a = int(input())
 for i in range(1, a+1):
     if i % 2 == 0 and i % 4 != 0:
         continue
-    elif i // 8 == 0 or i // 8 == 2 or i // 8 == 4:
+    # elif i // 8 == 0 or i // 8 == 2 or i // 8 == 4:
+    elif (i // 8) % 2 == 0:
         continue
     elif i % 7 < 4:
         continue
     else:
         print(i, end = ' ')
+
+    
