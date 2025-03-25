f38c007bc967132b78d2bf5b2a567ad8acb493a5
seohyeon <seohyeon0219@naver.com>
2025-03-25 22:33:48 +0900

    Title: 20대의 평균 나이, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250325/20대의 평균 나이/average-age-of-20.py
+++ 250325/20대의 평균 나이/average-age-of-20.py
@@ -3,9 +3,10 @@ cnt = 0
 
 while True:
     n = int(input())
-    if n >= 30:
-        break
-    else:
+    if n < 30:
         sum += n
         cnt += 1
+    else:
+        break
+        
 print(f'{sum / cnt :.2f}')
