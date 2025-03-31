adbe6b9d051f3a60ad07eaa033269c3d7aebd560
seohyeon <seohyeon0219@naver.com>
2025-03-31 17:08:39 +0900

    Title: 공약수의 유무, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250331/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 250331/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -3,11 +3,7 @@ satisfied = 0
 
 for i in range(a, b+1):
     if (1920 % i == 0) and (2880 % i == 0):
-        print(1)
-        satisfied += 1
-    else:
-        print(0)
-        satisfied += 1
+        satisfied = 1
+        break
 
-if satisfied >= 1:
-    break
+print(satisfied)
