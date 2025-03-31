afd0aa99d48497145a4b5c2138c044b551665309
seohyeon <seohyeon0219@naver.com>
2025-03-31 17:04:52 +0900

    Title: 공약수의 유무, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250331/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+satisfied = 0
+
+for i in range(a, b+1):
+    if (1920 % i == 0) and (2880 % i == 0):
+        print(1)
+        satisfied += 1
+    else:
+        print(0)
+        satisfied += 1
+
+if satisfied >= 1:
+    break
