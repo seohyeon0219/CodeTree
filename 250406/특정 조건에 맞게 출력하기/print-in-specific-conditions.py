747922a7e2f0161b773ebd27287b467797ea6232
seohyeon <seohyeon0219@naver.com>
2025-04-06 20:31:39 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250406/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,13 @@
+arr = list(map(int, input().split()))
+arr2 = []
+
+for elem in arr:
+    if elem == 0:
+        continue
+    elif elem % 2 == 1:
+        arr2.append(elem + 3)
+    else:
+        arr2.append(elem // 2)
+
+for elem in arr2:
+    print(elem, end=' ')
