a78f21f996d2e1e8e636d2a4b9d952bd77ec070c
seohyeon <seohyeon0219@naver.com>
2025-04-11 15:55:53 +0900

    Title: 1-9 개수 세기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/1-9 개수 세기/count-one-to-nine.py
+++ 250411/1-9 개수 세기/count-one-to-nine.py
@@ -1,9 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
-cnt_arr = [0] * 10
+cnt_arr = [0] * 9 # index : 0~8 총 9개
 
 for elem in arr:
-    cnt_arr[elem - 1] += 1
+    cnt_arr[elem-1] += 1 # 1은 0에 저장,,,,,9는 8에 저장
 
-for i in range(10): # 0 ~ 9 -> 10개
-    print(cnt_arr[i])
+for i in range(1, 10): # 0~9
+    print(cnt_arr[i-1])
