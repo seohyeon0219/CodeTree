d646c425864bf7695b9c28b9e67ac170920513e3
seohyeon <seohyeon0219@naver.com>
2025-04-17 20:51:46 +0900

    Title: 달에서 무게 구하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250417/달에서 무게 구하기/weight-on-the-moon.py
+++ 250417/달에서 무게 구하기/weight-on-the-moon.py
@@ -1,5 +1,5 @@
 weight = 13
 gravitation = 0.165
 
-print(f'{weight} * {gravitation} = {weight * gravitation : .4f}')
+print(f'{weight} * {gravitation : .6f} = {weight * gravitation : .6f}')
 # print(f'{weight} * {gravitation} = {result: .6f}')
