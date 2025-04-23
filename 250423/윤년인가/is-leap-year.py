ce2a860ad5c7b902a4a6b12781311a4f1c9b18ce
seohyeon <seohyeon0219@naver.com>
2025-04-23 12:48:14 +0900

    Title: 윤년인가, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250423/윤년인가/is-leap-year.py
+++ 250423/윤년인가/is-leap-year.py
@@ -1,6 +1,6 @@
 year = int(input())
 
-if year % 4 == 0 or (year % 100 == 0 and year % 400 != 0):
-    print('true')
-else:
+if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
     print('false')
+else:
+    print('true')
