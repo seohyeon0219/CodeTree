92d07ab0ac3e589345b52c2da633961ecb2aaef1
seohyeon <seohyeon0219@naver.com>
2025-04-30 11:36:03 +0900

    Title: 십의 자리 숫자, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/십의 자리 숫자/number-of-tens-digit.py
+++ 250430/십의 자리 숫자/number-of-tens-digit.py
@@ -1,7 +1,7 @@
 arr = list(map(int, input().split()))
 i = arr.index(0)
 arr = arr[:i]
-arr = [elem // 10 if elem >= 10 else elem for elem in arr]
+arr = [elem // 10 if elem >= 10 else 0 for elem in arr]
 cnt_arr = [0] * 10
 
 for elem in arr:
