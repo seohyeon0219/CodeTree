4e2f3e6132edfd81a5de6a6d05c060c5a5bcf530
seohyeon <seohyeon0219@naver.com>
2025-05-03 14:55:32 +0900

    Title: 문자 제거하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250503/문자 제거하기/del-char.py
+++ 250503/문자 제거하기/del-char.py
@@ -3,6 +3,10 @@ cnt = len(s)
 
 while cnt > 1:
     n = int(input())
-    cnt -= 1
-    s.pop(n)
-    print(''.join(s))
+    if n < len(s):
+        cnt -= 1
+        s.pop(n)
+        print(''.join(s))
+    if n >= len(s):
+        s.pop(-1)
+        print(''.join(s))
