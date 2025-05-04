7517699cb9f4294fdc1de55a608154eb9f59d0cc
seohyeon <seohyeon0219@naver.com>
2025-05-04 15:39:31 +0900

    Title: 대문자로 출력하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250504/대문자로 출력하기/print-in-capital.py
+++ 250504/대문자로 출력하기/print-in-capital.py
@@ -1,2 +1,11 @@
-s = input()
-print(s.upper())
+s = list(input())
+new_s = []
+
+for i in s:
+    if i.isalpha() == True:
+        new_s.append(i.upper())
+    else:
+        continue
+
+for i in new_s:
+    print(i, end='')
