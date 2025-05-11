b05916c46b6bdc564fc58357efe28f4a7bcbd654
seohyeon <seohyeon0219@naver.com>
2025-05-11 23:35:40 +0900

    Title: 실수 입력받아 계산, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250511/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split('\n');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let result = a + b;
+console.log(result.toFixed(2));
