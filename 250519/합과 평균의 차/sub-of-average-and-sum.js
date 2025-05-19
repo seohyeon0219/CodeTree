b5be5810e58c9cbd64ac95914eec09f3221b80d6
seohyeon <seohyeon0219@naver.com>
2025-05-19 15:16:01 +0900

    Title: 합과 평균의 차, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

--- 250519/합과 평균의 차/sub-of-average-and-sum.js
+++ 250519/합과 평균의 차/sub-of-average-and-sum.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(' ');
+
 let a = Number(input[0]);
 let b = Number(input[1]);
 let c = Number(input[2]);
 
 sum = a + b + c;
 average = sum / 3;
-console.log(sum + '\n' + average + '\n' + sum - average);
+
+console.log(sum);
+console.log(average);
+console.log(sum-average);
