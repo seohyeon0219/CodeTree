502ce6059026c95c8a3c353947490840358c4e74
seohyeon <seohyeon0219@naver.com>
2025-05-19 15:45:45 +0900

    Title: 체질량지수, Time: 169ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250519/체질량지수/bmi.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+let height = Number(input[0]);
+let width = Number(input[1]);
+
+let bmi = parseInt((10000 * width) / (height * height));
+
+if (bmi >= 25) {
+    console.log('Obesity');
+}
