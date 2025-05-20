130fb798f55137a60bdfd46c044288b346367b71
seohyeon <seohyeon0219@naver.com>
2025-05-20 10:58:11 +0900

    Title: 출석 부르기, Time: 185ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250520/출석 부르기/calling-attendance.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let number = fs.readFileSync(0).toString();
+
+if (number === 1) {
+    console.log('John');
+} else if (number === 2) {
+    console.log('Tom');
+} else if (number === 3) {
+    console.log('Paul');
+} else {
+    console.log('Vacancy');
+}
