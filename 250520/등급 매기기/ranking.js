5bf4484f0ee002ac2e823b05a29ea863a8478b1b
seohyeon <seohyeon0219@naver.com>
2025-05-20 11:00:39 +0900

    Title: 등급 매기기, Time: 183ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250520/등급 매기기/ranking.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let score = fs.readFileSync(0).toString();
+
+score = Number(score);
+
+if (score >= 90) {
+    console.log('A');
+} else if (score >= 80) {
+    console.log('B');
+} else if (score >= 70) {
+    console.log('C');
+} else if (score >= 60) {
+    console.log('D');
+} else {
+    console.log('F');
+}
