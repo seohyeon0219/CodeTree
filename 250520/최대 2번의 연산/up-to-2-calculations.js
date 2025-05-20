e6e84628fea3e80d2e5643d95bf3c481d73b480d
seohyeon <seohyeon0219@naver.com>
2025-05-20 11:12:00 +0900

    Title: 최대 2번의 연산, Time: 175ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250520/최대 2번의 연산/up-to-2-calculations.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let a = fs.readFileSync(0).toString().trim();
+
+if (a % 2 === 0) {
+    a = parseInt(a/2);
+} else if (a % 2 === 1) {
+    a = (a + 1) / 2
+}
+
+console.log(a.toFixed(0));
