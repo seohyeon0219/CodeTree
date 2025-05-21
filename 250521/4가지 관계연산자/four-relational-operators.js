7b865ff6758679977aadf189b0df477895bd7ef5
seohyeon <seohyeon0219@naver.com>
2025-05-21 17:46:02 +0900

    Title: 4가지 관계연산자, Time: 162ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250521/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(' ');
+
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+if (a >= b) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+
+if (a > b) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+
+if (b >= a) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+
+if (b > a) {
+    console.log(1);
+} else {
+    console.log(0);
+}
