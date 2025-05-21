4ebaf58ebec3f20610bc14af51074aa3ae955894
seohyeon <seohyeon0219@naver.com>
2025-05-21 17:41:14 +0900

    Title: 3 또는 5의 배수, Time: 166ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250521/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let a = fs.readFileSync(0).toString();
+
+a = Number(a);
+
+if (a % 3 === 0) {
+    console.log('YES');
+} else {
+    console.log('NO');
+}
+
+if (a % 5 === 0) {
+    console.log('YES');
+} else {
+    console.log('NO');
+}
